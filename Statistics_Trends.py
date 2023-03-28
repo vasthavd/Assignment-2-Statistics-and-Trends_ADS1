228d200e5bebd4ab010e8c5fc0d25096a526fb5c
Vasthav Dandumenu <vasthavdandumenu143@gmail.com>
2023-03-28 23:30:19 +0100

    Corr added Statistics_Trends.py
    Added corelation plots and describe methods as this update

--- Statistics_Trends.py
+++ Statistics_Trends.py
@@ -8,7 +8,9 @@ Created on Tue Mar 21 12:22:35 2023
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import seaborn as sns
+import cluster_tools as ct
+import stats as st
 
 def read_world_bank_csv(filename):
     # Read the CSV file into a DataFrame
@@ -32,25 +34,25 @@ data, area = read_world_bank_csv('Electricity.csv')
 print(data.head())
 print(area.head())
 
-
-# Select the 5 countries to plot
-countries = ['United States', 'China', 'India', 'Japan']
+# Percentage of electricity
+# Select the 10 countries to plot
+countries = ['United States', 'China', 'Japan', 'Germany',  'India', 
+             'United Kingdom', 'France', 'Brazil', 'Italy', 'Canada']
 
 # Get the data for these countries
 data_countries = data.loc[countries]
 
 # Plot the data
-plt.figure(figsize=(10, 6)) # Set the size of the figure
+plt.figure(figsize=(10, 5)) # Set the size of the figure
 for country in countries:
     plt.plot(data.columns, data_countries.loc[country], label=country)
 plt.xlabel('Year') # Set the label for the x-axis
 plt.xticks(rotation=90)
-plt.locator_params(axis='x', nbins=4)
 plt.ylabel('% of population with access to electricity') # Set the label for the y-axis
 plt.title('Access to Electricity by Country') # Set the title for the plot
 plt.legend() # Show the legend
 
-
+#urban population
 
 up, up_t = read_world_bank_csv('urbanp.csv')
 
@@ -59,11 +61,11 @@ up, up_t = read_world_bank_csv('urbanp.csv')
 up_countries = up.loc[countries]
 
 # Plot the data
-plt.figure(figsize=(10, 6)) # Set the size of the figure
+plt.figure(figsize=(7, 5)) # Set the size of the figure
 for country in countries:
     plt.plot(up.columns, up_countries.loc[country], label=country)
 plt.xlabel('Year') # Set the label for the x-axis
-#plt.xlim('1960', '2021')
+plt.xlim(min(up.columns), max(up.columns))
 plt.ylabel('Urban population') # Set the label for the y-axis
 plt.xticks(np.arange(0, len(up.columns)+1, 5), rotation = 90)
 #plt.xticks(rotation = 90)
@@ -71,4 +73,41 @@ plt.title('Population') # Set the title for the plot
 
 plt.legend() # Show the legend
 #plt.xticks(np.arange(up.columns,10)) 
+
+
+#methane
+
+methane, methane_t = read_world_bank_csv('Methane.csv')
+
+
+# Extract the data for the 5 countries
+#countries = ['China', 'United States', 'India', 'Canada', 'Japan']
+years = ['2015', '2016', '2017', '2018', '2019']
+usage = up.loc[countries, years]
+
+# Plot the bar plot
+usage.plot(kind='bar')
+plt.xlabel('Country')
+plt.ylabel('Electricity usage')
+plt.title('Electricity usage in 5 countries from 2015 to 2019')
+
+
+print(methane.describe())
+print(usage)
+
+
+fd = pd.merge(data['2015'], up['2015'], left_index=True, right_index=True)
+fd = pd.merge(fd, methane['2015'], left_index=True, right_index=True)
+fd.columns = ['Electricity', 'Urbanp', 'Methane']
+# Show the merged dataframe
+print(fd)
+plt.figure()
+corr_matrix = fd.corr(method='pearson')
+
+# Plot the correlation heatmap using Seaborn
+sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')
+
+
+
 plt.show()
+
