30405bb531e35dac2d68e65be18b445ecfcb9634
Vasthav Dandumenu <vasthavdandumenu143@gmail.com>
2023-04-03 15:00:01 +0100

    Update Statistics_Trends.py
    Finalized two functions

--- Statistics_Trends.py
+++ Statistics_Trends.py
@@ -8,45 +8,121 @@ Created on Tue Mar 21 12:22:35 2023
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import stats as st
 
-def read_world_bank_csv(filename):
-    # Read the CSV file into a DataFrame
-    df = pd.read_csv(filename, header=2)
-    df = df.drop(['Indicator Code', 'Country Code', 'Indicator Name'], axis=1)
-    df.dropna()
-    df.dropna(how='all', axis=1, inplace=True)
 
-    df2 = df.transpose()
-    df2.columns = df2.iloc[0]  # Set the first row as the column names
-    df = df.set_index('Country Name')
-    #df2 = df2.set_index(('Country Name', ''), append=True)
-    #df2 = df2.rename_axis('Year')
-    df2 = df2.rename_axis(columns='Country Name', index='Year')
-    df = df.rename_axis(index='Country Name', columns='Year')
+def read_world_bank_csv(file_name):
+    
+    """
+    This function reads a CSV file from the World Bank 
+    and returns two DataFrames.
 
-    df2 = df2.iloc[1:]  # Drop the first row
-    return df, df2
+    Parameters
+    ----------
+    file_name : str
+    The path to the CSV file.
 
-def group_years_by_count(df):
+    Returns
+    -------
+    year_as_column : DataFrame
+    The DataFrame containing the data from the CSV file with years as columns.
+
+    country_as_column : DataFrame
+    The DataFrame containing the data from the CSV file, transposed with 
+    countries as columns.
+
+    Raises
+    ------
+    FileNotFoundError
+    If the CSV file does not exist.
+
+    IOError
+    If there is an error reading the CSV file.
+
+    """
+    # Reading the CSV file into a DataFrame
+    year_as_column = pd.read_csv(file_name, header=2)
+
+    # Dropping the first three columns, which are the header, 
+    # Indicator Code, and Country Code.
+    year_as_column = year_as_column.drop(['Indicator Code', 'Country Code',
+                                          'Indicator Name'], axis=1)
+
+    # Dropping any rows that contain missing values and cleaning dataframe.
+    year_as_column.dropna()
+    year_as_column.dropna(how='all', axis=1, inplace=True)
+    
+    # Setting the index of the DataFrame to the Country Name column.
+    year_as_column = year_as_column.set_index('Country Name')
+
+    # Renaming the axis of the DataFrame to Years.
+    year_as_column = year_as_column.rename_axis(index='Country Name',
+                                                columns='Year')
+
+    # Transposing the DataFrame.
+    country_as_column = year_as_column.transpose()
+
+    # Setting the first row of the DataFrame as the column names.
+    country_as_column.columns = country_as_column.iloc[0]
+
+    # Renaming the axis of the DataFrame to Countries.
+    country_as_column = country_as_column.rename_axis(columns='Country Name', 
+                                                      index='Year')
+
+    # Dropping the first row of the DataFrame, which is just the column names.
+    country_as_column = country_as_column.iloc[1:]
+
+    # Returns the two DataFrames.
+    return year_as_column, country_as_column
+
+def group_years_by_count(year_as_column_df):
+    
+    """Group years based on non-null count.
+
+    This function takes a pandas dataframe as input, and creates a new dataframe
+    with non-null count and year columns. The years are then grouped into two
+    categories based on whether their non-null count is above or below the median
+    count. The resulting groups are printed with a message indicating the count,
+    which is useful to determine years selection for analysis.
+
+    Parameters
+    ----------
+    year_as_column_df : pandas dataframe
+        A dataframe with one column containing year data.
+
+    Returns
+    -------
+    result: str
+        A string with two sections separated by a blank line. The first section
+        lists the years with non-null count above the median count, and the
+        second section lists the years with non-null count less than or equal to
+        the median count.
+    """
+    
     # Create dataframe with non-null count and year columns
-    fda_df = df.notnull().sum().sort_values(ascending=False).to_frame().reset_index()
+    fda_df = year_as_column_df.notnull().sum().sort_values(ascending=False)\
+        .to_frame().reset_index()
     fda_df.columns = ['Year', 'Non-null Count']
 
     # Calculate median non-null count
     median_count = fda_df['Non-null Count'].median()
 
     # Group years based on non-null count above or below median
-    fda_groups = fda_df.groupby(fda_df['Non-null Count'] > median_count).apply(lambda x: x['Year'].tolist())
+    fda_groups = fda_df.groupby(fda_df['Non-null Count'] > median_count)\
+        .apply(lambda x: x['Year'].tolist())
 
     # Print groups with messages indicating count
     result = []
-    result.append(f"Years with non-null count above the median count ({median_count}):")
+    result.append(
+        f"Years with non-null count above the median count ({median_count}):")
     result.append(str(fda_groups[True]))
     result.append("")
-    result.append(f"Years with non-null count less than or equal to the median count ({median_count}):")
+    result.append(
+        f"Years with non-null count less than or equal to the median count ({median_count}):")
     result.append(str(fda_groups[False]))
     return '\n'.join(result)
+
+
+
 
     
 # Indicator : Access to Electricity(% of Population)
